d229e23242b266fe559cf5887d77581d95f6dfab
Lucas <lucassilvaee1245@gmail.com>
2025-02-10 22:56:28 -0300

    test

--- src/pages/Site.tsx
+++ src/pages/Site.tsx
@@ -1,12 +1,15 @@
 import { Box, Flex, Image, Button, Heading, Text, Input, VStack, HStack, useBreakpointValue } from "@chakra-ui/react";
 import '../../src/css/css.css'
+import Mapa from "../componets/Mapa";
+import MecanicosParceiros from "../componets/MecanicosParceitos";
+import { Apoiadores } from "../componets/Apoiadores";
 
 const HeaderComponent = () => {
   // Para alternar o display do menu em telas menores
   const isMobile = useBreakpointValue({ base: true, md: false });
 
   return (
-    <Box bg="#040c18" color="white" minHeight="100vh" display="flex" flexDirection="column" justifyContent="center" alignItems="center" px={4}>
+    <Box bg="#040c18" color="white" minHeight="100vh" display="flex" flexDirection="column" justifyContent="center" alignItems="center" >
       {/* Navegação */}
       <Flex
       borderBottom="1px solid white" 
@@ -17,7 +20,7 @@ const HeaderComponent = () => {
         px={{ base: 4, md: 40 }} // Responsivo: menos padding em telas menores
         justify="space-between"
         align="center"
-        position="absolute"
+      
         top={0}
         left={0}
         right={0}
@@ -34,17 +37,17 @@ const HeaderComponent = () => {
             <Button colorScheme="white" variant="link" mr={4}>Eventos</Button>
             <Button colorScheme="white" variant="link" mr={4}>Fale Conosco</Button>
             <Button colorScheme="white" variant="link" mr={4}>Galeria de Fotos</Button>
-            <Button  fontWeight={'normal'} colorScheme="red" variant="solid" mr={4}>Quero ser Membro</Button>
-            <Button  fontWeight={'normal'} colorScheme="blue" variant="solid">Login</Button>
+            <Button  fontWeight={'normal'} colorScheme="blue" variant="solid" mr={4}>Quero ser Membro</Button>
+            <Button  fontWeight={'normal'} colorScheme="red" variant="solid">Login</Button>
           </Flex>
         )}
       </Flex>
 
       {/* Conteúdo Principal */}
-      <Box w={{ base: "90%", sm: "70%", md: "50%" }} textAlign="center" py={16}>
+      <Box w={{ base: "90%", sm: "70%", md: "50%" }} textAlign="center" pb={6} pt={16}>
         <Heading
           as="h1"
-          size="3xl"
+          size="4xl"
           mb={4}
           fontFamily={"Roboto, sans-serif"}
           background="linear-gradient(to right, #357398, #91A5B1)"
@@ -58,7 +61,33 @@ const HeaderComponent = () => {
         </Text>
 
         {/* Seção de Input de E-mail */}
-        <HStack spacing={4} justify="center">
+ {isMobile ? <>
+  <VStack spacing={4} justify="center">
+          <Input
+          fontFamily={"Roboto, sans-serif"}
+            placeholder="Digite seu email"
+            size="lg"
+            width="full"
+            maxWidth="400px"
+            bg="#052d56"
+            color="#052d56"
+            borderColor="#031f3a"
+            focusBorderColor="#031f3a"
+            fontWeight={'normal'}
+          />
+          <Button
+          fontFamily={"Roboto, sans-serif"}
+            colorScheme="red"
+            fontWeight={'normal'}
+            size="lg"
+            width={{ base: "full", md: "auto" }} // Ajusta a largura do botão em telas pequenas
+            maxWidth="250px"
+          >
+            Quero ser Premium
+          </Button>
+        </VStack>
+ </> : <>
+ <HStack  justify="center">
           <Input
           fontFamily={"Roboto, sans-serif"}
             placeholder="Digite seu email"
@@ -82,7 +111,11 @@ const HeaderComponent = () => {
             Quero ser Premium
           </Button>
         </HStack>
+ </>}
       </Box>
+      <MecanicosParceiros/>
+      <Apoiadores/>
+      <Mapa/>
     </Box>
   );
 };
